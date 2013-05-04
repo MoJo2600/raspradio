097283fda4382c1f671edcee38fdf21073ccb8f3
Christian Erhardt <mail@mojo2k.de>
2013-05-04 20:27:29 +0000

    input is not working :(

--- radio.py
+++ radio.py
@@ -5,7 +5,10 @@ import os
 import signal
 import sys
 import threading
+import datetime
 from time import sleep
+from thread import start_new_thread
+from Queue import Queue
 
 # Zuordnung der GPIO Pins (ggf. anpassen)
 DISPLAY_RS = 7
@@ -33,59 +36,158 @@ def signal_handler(signal, frame):
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
 class InternetRadio:
 
 
 	def __init__(self):
 		self.current_station = ""
 		self.current_char = 0
-		self.playlist = {}
+		self.radio_stations = []
 		self.init_playlist()
+		self.current_station_index = 1
+		self.refresh_suspended_till = datetime.datetime.now()
+		self.queue = Queue()
 
 	def init_playlist(self):
-		f=os.popen("radiostations.txt")
-		i = 1
-		for station_raw in f.readlines():
-			parts = station_raw.split("|")			
-			station = { index: i,
-				    name: parts[0],
-				    url: parts[1]
-				    } 			
-			self.playlist.append(station)
-			i += 1
 		# clear mpc playlist
-		os.system("mpc "		
-		
+		os.system("mpc clear")
 
-	def show_station(self):
+		stations_file = os.path.join(os.getcwd(), "radiostations.txt")
+		f=open(stations_file, 'r')
+		i = 1
+		for station_raw in f:
+			parts = station_raw.split("|")			
+			station = { 'index': i,
+				    'name': parts[0],
+				    'url': parts[1]
+				    } 			
+			self.radio_stations.append(station)
+			i += 1
+
+			# add stations to mpc playlist
+			os.system("mpc add %s" % station['url'])		
+
+		# start playing immediately
+		self.current_station_index = 1
+		os.system("mpc play")
+		
+	def _clear_display(self):
+		lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)			
+		lcd_string(STR_PAD)
+		lcd_byte(DISPLAY_LINE_2, DISPLAY_CMD)			
+		lcd_string(STR_PAD)
+
+	def next(self):
+		self.refresh_suspended_till = datetime.datetime.now() + datetime.timedelta(seconds=5)
+		self.current_station_index += 1
+		if self.current_station_index > len(self.radio_stations):
+			self.current_station_index = 1
+
+		self._clear_display()
+
+		print "next"
+
+	def previous(self):
+		self.refresh_suspended_till = datetime.datetime.now() + datetime.timedelta(seconds=5)
+		self.current_station_index -= 1
+		if self.current_station_index < 1:
+			self.current_station_index = len(self.radio_stations)
+
+		self._clear_display()
+
+		self._show_track_indicator()
+		print "previous"
+
+	def _show_station_worker(self):
+		while 1:
+			self._show_station()
+			try:
+				item = self.queue.get_nowait()
+				if item == "next":
+					self.next()
+				if item == "previous":
+					self.previous()
+				self.queue.task_done()
+			except:
+				pass
+#			time.sleep(0.3)
+
+	def _read_input_worker(self):
+		print "input"
+		getch = _GetchUnix()
+		while 1:
+			key = getch()
+			if key == "n":				
+				self.queue.put("next")
+			if key == "p":
+				self.queue.put("previous")
+
+	def run(self):
+		print "starting display thread"
+		t1 = threading.Thread(target=self._show_station_worker())
+#		t1.daemon = True
+		t1.start()
+		print "starting input thread"
+		t2 = threading.Thread(target=self._read_input_worker())
+#		t2.daemon = True
+		t2.start()
+		print "all done"
+
+
+	def _show_track_indicator(self):
+		lcd_byte(DISPLAY_LINE_2, DISPLAY_CMD)			
+		track_numbers = "%d/%d" % (self.current_station_index,len(self.radio_stations)) 		
+		track_text = " " * (DISPLAY_WIDTH - len(track_numbers)) 
+		track_text += track_numbers
+		lcd_string(track_text)
+
+
+	def _show_station(self):
+#		print "%s, %s" % (self.refresh_suspended_till, datetime.datetime.now())
+		if self.refresh_suspended_till > datetime.datetime.now():
+			return
+
 		f=os.popen("mpc current")	
+
 		station_text = ""
 		for i in f.readlines():
         		station_text += i
 
-		if station_text != self.current_station:
-			self.current_station = station_text
+		if self.current_station != station_text:
+			# reset view if current track changes
 			lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)
 			lcd_string(STR_PAD)
 			self.current_char = 0
+			self.current_station = station_text
 
 		lcd_text = self.current_station[self.current_char:(self.current_char+16)]
 		lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)
 		lcd_string(lcd_text)
 
-		lcd_byte(DISPLAY_LINE_2, DISPLAY_CMD)
-		index = self.playlist.index(station_text)
-		track_text = "%s/%s" % (index,len(self.playlist)) 
-		track_text = " " * (DISPLAY_WIDTH - len(track_text)) + track_text
-
-		lcd_string(track_text)
+		# show track indicator
+		self._show_track_indicator()
 
 		self.current_char += 1
-		if self.current_char > DISPLAY_WIDTH:
+		if self.current_char > len(self.current_station):
 			self.current_char = 0
 
-		sleep(0.3)
-		self.show_station()
 
 def main():
 	GPIO.setmode(GPIO.BCM)
@@ -110,20 +212,19 @@ def main():
 	#lcd_byte(DISPLAY_LINE_2, DISPLAY_CMD)
 	#lcd_string("funktioniert! :)")	
 
-	f=os.popen("mpc current")
-	station=""
-	for i in f.readlines():
-		station += i
-	lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)
-	lcd_string(station)
+#	f=os.popen("mpc current")#
+#	station=""
+#	for i in f.readlines():
+#		station += i
+#	lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)
+#	lcd_string(station)
 
 	radio = InternetRadio()
 
 #        threading.Timer(1.0, radio.show_station()).start()
-	radio.show_station()		
-#	while 1: #
-#		print "looping"
-#		time.sleep(20)
+	radio.run()
+#        start_new_thread(radio.readinput())
+
 
 
 def display_init():
