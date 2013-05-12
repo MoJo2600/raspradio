e798d8bd471892cbd8bf5d380f9ea898aaa12f85
Christian Erhardt <mail@mojo2k.de>
2013-05-12 20:23:27 +0000

    radion and encoder working

--- RotaryEncoder.py
+++ RotaryEncoder.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 
 ROTARY_A_PIN = 0
 ROTARY_B_PIN = 0
@@ -8,6 +9,12 @@ ROTARY_EVENT = GPIO.RISING
 BOUNCETIME = 30
 
 def rotate_callback(channel):
+	"""
+	callback function for the input pin interrupt
+
+	had to place it in a global function, because i had AttributeErrors when
+	it was implemented in a class - lock necessary? hm...
+	"""
 	global ROTARY_A_PIN
 	global ROTARY_B_PIN
 	global SET_COUNT
@@ -16,7 +23,6 @@ def rotate_callback(channel):
 	GPIO.remove_event_detect(channel)
 	a = GPIO.input(channel)
 	b = GPIO.input(ROTARY_B_PIN)
-	print ('%s %s' % (a, b))
 	if a == b:
 		SET_COUNT -= 1
 	else:
@@ -32,15 +38,18 @@ class RotaryEncoder:
 	def __init__(self, a_pin, b_pin, sw_pin):
 		global ROTARY_A_PIN
 		global ROTARY_B_PIN
+
 		ROTARY_A_PIN = a_pin
 		ROTARY_B_PIN = b_pin
+
+		self.lock = threading.Lock()
 		self.sw_pin = sw_pin
 		self.rotary_step = 0
 		self._event = GPIO.RISING
 		self._bouncetime = 30
 
+		# Setup all used ports
 		GPIO.setmode(GPIO.BCM)
-
 		GPIO.setup(ROTARY_A_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.setup(ROTARY_B_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #		GPIO.setup(self.sw_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -50,19 +59,7 @@ class RotaryEncoder:
 	def _button_callback(self, channel):
 		print "button pressed"
 
-
 	def get_steps(self):
-		return SET_COUNT
-	
-
-#encoder = RotaryEncoder(4, 3, 2)
-#last_step = 0
-#while 1:
-#	rotary_step = encoder.get_steps()
-#	if last_step > rotary_step:		
-#		print(">")
-#		last_step = rotary_step
-#	elif last_step < rotary_step:
-#		print("<")
-#		last_step = rotary_step
-#	time.sleep(0.1)
+		with self.lock:
+			count = SET_COUNT
+		return count
--- radio.py
+++ radio.py
@@ -5,6 +5,7 @@ import os
 import signal
 import sys
 import threading
+import Queue
 import datetime
 from time import sleep
 from RotaryEncoder import RotaryEncoder
@@ -29,28 +30,60 @@ STR_PAD = " " * 16
 CURRENT_STATION = ""
 CURRENT_CHAR = 0
 
+LAST_STATION_FILE = ".laststation"
+
+WAIT_FOR_STATION_CHANGE = 200	# Wati 2 sek before switcheng stations
+
 def signal_handler(signal, frame):
+	"""
+	clean up before closing application
+	"""
         print 'Closing Internet Radio!'
 	GPIO.cleanup()
         sys.exit(0)
+	MPCClient.command_queue.join()
+# Connect singal_handler callback to sigint
 signal.signal(signal.SIGINT, signal_handler)
 
+class MPCClient(threading.Thread):
+	"""
+	this class handles the mpc commands in an own thread
+	"""
+	CMD_CLEAR = "mpc clear"
+	CMD_ADD = "mpc add %s"
+	CMD_PLAY = "mpc play"
+	CMD_PLAY_INDEX = "mpc play %s"
+	
+	command_queue = Queue.Queue()
+	lock = threading.Lock()
+	
+	def run(self):
+		while True:
+			command = MPCClient.command_queue.get()
+			os.system(command)
+			MPCClient.command_queue.task_done()		
 
 class InternetRadio:
-
 
 	def __init__(self):
 		self.current_station = ""
 		self.current_char = 0
 		self.radio_stations = []
-		self.init_playlist()
-		self.current_station_index = 1
 		self.refresh_suspended_till = datetime.datetime.now()
 		self._switch_station = False
 
+		self.mpcclient = MPCClient()
+		self.mpcclient.setDaemon(True) 
+		self.mpcclient.start()
+
+		self.init_playlist()
+
 	def init_playlist(self):
+		"""
+		loads all stations from radiostations.txt into mpd playlist
+		"""
 		# clear mpc playlist
-		os.system("mpc clear")
+		MPCClient.command_queue.put(MPCClient.CMD_CLEAR)
 
 		stations_file = os.path.join(os.getcwd(), "radiostations.txt")
 		f=open(stations_file, 'r')
@@ -65,11 +98,23 @@ class InternetRadio:
 			i += 1
 
 			# add stations to mpc playlist
-			os.system("mpc add %s" % station['url'])		
+			MPCClient.command_queue.put(MPCClient.CMD_ADD % station['url'])
 
 		# start playing immediately
 		self.current_station_index = 1
-		os.system("mpc play")
+		try:
+			with open(LAST_STATION_FILE) as f: 
+				index = int(f.read())
+				if index > 0 and index <= len(self.radio_stations):
+					self.current_station_index = index
+		except IOError:
+			print 'No last station file, playing first'
+		except ValueError:
+			print 'last station file corrupted'
+			os.remove(LAST_STATION_FILE)
+
+		self._show_track_indicator()
+		MPCClient.command_queue.put(MPCClient.CMD_PLAY_INDEX % self.current_station_index)
 		
 	def _clear_display(self):
 		lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)			
@@ -79,41 +124,47 @@ class InternetRadio:
 
 	def next(self):
 		self.refresh_suspended_till = datetime.datetime.now() + datetime.timedelta(seconds=5)
+
 		self.current_station_index += 1
 		if self.current_station_index > len(self.radio_stations):
 			self.current_station_index = 1
 
 		self._clear_display()
-
 		self._show_track_indicator()
-
 		self._switch_station = True
 		print("next")
 
 	def previous(self):
 		self.refresh_suspended_till = datetime.datetime.now() + datetime.timedelta(seconds=3)
+
 		self.current_station_index -= 1
 		if self.current_station_index < 1:
 			self.current_station_index = len(self.radio_stations)
 
 		self._clear_display()
-
 		self._show_track_indicator()
-
 		self._switch_station = True
 		print("previous")
+
+	def _store_last_station(self):
+		"""
+		save the last selected radio station for convenience
+		"""
+		f=open(LAST_STATION_FILE, 'w')
+		f.write("%s" % self.current_station_index)
 
 	def run(self):
                 self._show_track_indicator()
 		encoder = RotaryEncoder(3, 4, 2)		
 		last_step = 0
 		while 1:
-			# own thread
 			if self._switch_station and self.refresh_suspended_till < datetime.datetime.now():
 				self._switch_station = False
-#				os.system("mpc play %s" % self.current_station_index)
+				MPCClient.command_queue.put(MPCClient.CMD_PLAY_INDEX % self.current_station_index)
+				self._store_last_station()
 
 		        rotary_step = encoder.get_steps()
+
 			# debounce! switch only all 100ms or so
 		        if rotary_step > last_step:
 				self.next()
@@ -124,22 +175,21 @@ class InternetRadio:
 
 			self._show_station()
 
-#			time.sleep(0.1)
+			time.sleep(0.001)
 
 	def _show_track_indicator(self):
 		lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)
 		lcd_string(self.radio_stations[self.current_station_index-1]['name'])
 		lcd_byte(DISPLAY_LINE_2, DISPLAY_CMD)			
-		track_numbers = "%d/%d" % (self.current_station_index,len(self.radio_stations)) 		
+		track_numbers = "%d/%d" % (self.current_station_index, len(self.radio_stations)) 		
 		track_text = " " * (DISPLAY_WIDTH - len(track_numbers)) 
 		track_text += track_numbers
 		lcd_string(track_text)
 
 	def _show_station(self):
-#		print "%s, %s" % (self.refresh_suspended_till, datetime.datetime.now())
 		if self.refresh_suspended_till > datetime.datetime.now():
 			return
-
+		# TODO should be implemented in MPCClient
 		f=os.popen("mpc current")	
 
 		station_text = ""
@@ -161,7 +211,7 @@ class InternetRadio:
 		if self.current_char > len(self.current_station):
 			self.current_char = 0
 
-		self.refresh_suspended_till = datetime.datetime.now() + datetime.timedelta(milliseconds=300)
+		self.refresh_suspended_till = datetime.datetime.now() + datetime.timedelta(milliseconds=WAIT_FOR_STATION_CHANGE)
 
 
 def main():
@@ -175,32 +225,8 @@ def main():
 
 	display_init()
 
-	#lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)
-	#lcd_string("Schnatterente")
-	#lcd_byte(DISPLAY_LINE_2, DISPLAY_CMD)
-	#lcd_string("Nak nak nak!")
-
-	#time.sleep(5)
-
-	#lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)
-	#lcd_string("Dein Display")
-	#lcd_byte(DISPLAY_LINE_2, DISPLAY_CMD)
-	#lcd_string("funktioniert! :)")	
-
-#	f=os.popen("mpc current")#
-#	station=""
-#	for i in f.readlines():
-#		station += i
-#	lcd_byte(DISPLAY_LINE_1, DISPLAY_CMD)
-#	lcd_string(station)
-
 	radio = InternetRadio()
-
-#        threading.Timer(1.0, radio.show_station()).start()
 	radio.run()
-#        start_new_thread(radio.readinput())
-
-
 
 def display_init():
 	lcd_byte(0x33,DISPLAY_CMD)
